df143834fa1df42dcd70168005bce91d520b4e4a
BrendaCav14 <111449881+BrendaCav14@users.noreply.github.com>
2022-09-12 00:55:42 -0500

    Paginas nuevas
    html-css-js

---
+++ TIENDA/Producto.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php echo  "<title>PRODUCTO</title>"?>
+
+    <link rel="stylesheet" href="Perfil.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+      
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   
+  
+</head>
+<body>
+
+<div class="container">
+<div class="main-body">
+     <!-- Breadcrumb -->
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <ol >
+            <a class="navbar-brand" href="LandingPage.php" style="color:white; font-family: Sofia, sans-serif; font-size:40px; margin-top:-15px" >Shopping ♥</a>
+            <a class="tucuenta" style="color:white">Tu cuenta: </a>
+            <a class="tucuenta2" style="color:white" >Kenneth56 </a>
+            <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Avatar" class="avatar">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="margin-left:950px;">
+    <a class="dropdown-item" href="#" >Configuracion</a>
+    <a class="dropdown-item" href="#" >Editar Perfil</a>
+    <a class="dropdown-item" href="#" >Cerrar Sesion</a>
+  </div>
+            </ol>
+          </nav>
+
+        <div style="background-color:white; width:800px; height: 500px; margin-left:5px;">
+            <h2>HP Elite ordenador de sobremesa Intel Core i5 de 3,1 GHz,</h2>
+            <h2>8 gb Ram, 1 TB de disco duro, DVDRW, monitor LCD de 19 pulgadas,</h2>
+            <h2>teclado, ratón, WiFi inalámbrico, Windows 10 (renovado)</h2>
+       <br></br>
+
+       <div class="container" style="margin-top:10px;width:600px;height:300px">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" >
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+    <div class="item active" style="width:600px;height:300px">
+      <img src="https://m.media-amazon.com/images/I/718sn7oOcfL._AC_SL1500_.jpg" alt="Imagen1" style="margin-left:100px; width:400px; height:300px;">
+    </div>
+
+    <div class="item" style="width:600px;height:300px">
+      <img src="https://m.media-amazon.com/images/I/71I05GnbEZL._AC_SL1500_.jpg" alt="Imagen2"  style="margin-left:100px; width:400px; height:300px;">
+    </div>
+
+    <div class="item" style="width:600px;height:300px">
+      <img src="https://m.media-amazon.com/images/I/61VGE47mYYL._AC_SL1500_.jpg" alt="Imagen3" style="margin-left:100px; width:400px; height:300px;">
+    </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+       <br></br>
+
+       <h4>El producto está reacondicionado, es totalmente funcional y se encuentra en excelente estado.</h4>
+       <h4> Respaldado por la garantía de 90 días.</h4>
+
+
+
+          <div style="background-color:white; width:300px; height: 500px; margin-left:810px; margin-top:-100px">
+          <h1>hola</h1>
+          </div>
+        </div>
+
+</div>
+</div>
+
+
+
+</body>
+</html>
